a0ac52c4703424af86ef78c74c6d2c40c2228bf9
SanduSewwandi <sandunis17sewwandi@gmail.com>
2026-02-03 19:59:48 +0530

    new2

--- frontEnd/src/Components/Pages/Adminpage.jsx
+++ frontEnd/src/Components/Pages/Adminpage.jsx
@@ -30,6 +30,7 @@ import {
   Target,
 } from "lucide-react"
 import "./Adminpage.css"
+import AddPlant from "./AddPlant"
 
 export default function AdminPage() {
   const [activeSection, setActiveSection] = useState("dashboard")
